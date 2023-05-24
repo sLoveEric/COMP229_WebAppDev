2211cfdf4b6c805912ef8f67e1e826ca089803b7
Sean Wang <s.wang.619@gmail.com>
2023-05-24 16:51:18 -0400

    add week3 express ejs

--- Week2/server.js
+++ Week2/server.js
@@ -1,5 +1,41 @@
-// const hello = require('./hello');
-// hello.sayHello();
+// const http = require('http');
+// http.createServer((req, res) => {
+//     res.writeHead(200, {'Content-Type': 'text/plain'
+//     });
+//     res.end('Hello World');
+//     }).listen(3000);
+// console.log('Server running at http://localhost:3000/');
 
-const hello = require('./hello');
-hello();
+//=======================================================
+const connect = require('connect');
+const app = connect();
+
+function logger(req, res, next){
+    console.log(req.method, req.url);
+    next();
+}
+
+function helloWorld(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World Again');
+};
+
+function goodbyeWorld(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Goodbye World Again');
+};
+
+function notFound(req, res, next) {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not Found');
+};
+
+app.use(logger);
+app.use('/hello',helloWorld);
+app.use('/goodbye',goodbyeWorld);
+app.use(notFound); // The notFound middleware will handle any other requests
+
+app.listen(3000); // Starts the server
+
+console.log('Server running at http://localhost:3000/');
